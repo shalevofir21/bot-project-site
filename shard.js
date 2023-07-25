df92da53677ad01baa1287d69b6dffde086b1698
shalev1111 <50710665+shalevofir21@users.noreply.github.com>
2023-07-25 14:17:13 +0300

    Update shard.js

--- shard.js
+++ shard.js
@@ -1,5 +1,5 @@
 const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./index.js', { token: 'Nzk2OTg4MzA3NDYwNDU2NTA4.X_f7Mw.HrZ86XUy2L8tNYXMy0dFa0n125I '});
+const manager = new ShardingManager('./index.js', { token: ''});
 
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
 manager.spawn();
